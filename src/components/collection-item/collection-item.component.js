5fd117d494b2ac4bc80ca5e5a0b92b8b099f56a6
Lana Kirilenko <lana.kirilenko@vida.co.uk>
2021-01-19 12:41:06 +0000

    collection item

---
+++ src/components/collection-item/collection-item.component.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './collection-item.styles.scss'
+
+
+const CollectionItem = ({id, price, name, imageUrl}) => (
+    <div className="collection-item">
+        <div 
+            className="image"
+            style={{backgroundImage: `url(${imageUrl})`}}
+        />
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+        
+    </div>
+)
+
+export default CollectionItem;
---
+++ src/components/shop/shop-page.component.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import SHOP_DATA from "./shop.data";
+
+import '../../components/preview-collection/preview-collection.component'
+import CollectionPreview from '../../components/preview-collection/preview-collection.component';
+
+class ShopPage extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            collections: SHOP_DATA
+        }
+    }
+
+    render() {
+        const { collections } = this.state
+        return (
+            <div className="shop-page">
+                {
+                    collections.map(({id, ...collectionProps }) => (
+                       <CollectionPreview key={id} {...collectionProps} />
+                    ))
+                }
+            </div>
+        )
+    }
+}
+
+export default ShopPage;
