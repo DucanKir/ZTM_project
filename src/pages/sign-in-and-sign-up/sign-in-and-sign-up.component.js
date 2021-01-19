9b50fa6995d53c2f75d3676905a1c3bb7a5ea484
Lana Kirilenko <lana.kirilenko@vida.co.uk>
2021-01-19 13:38:28 +0000

    sign in page

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './sign-in-and-sign-up.styles.scss';
+import SingnIn from '../../components/sign-in/sign-in.component';gi add
+
+const SignInAndSignUpPage = () => (
+    <div className="sign-in-sign-up">
+        <SingnIn />
+    </div>
+)
+
+export default SignInAndSignUpPage;
